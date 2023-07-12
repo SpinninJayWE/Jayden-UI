d774ba884ef159c3f78e99903ef6d30a6602311b
SpinninJayWE <1315025826@qq.com>
2023-07-12 21:16:59 +0800

    1. 编写modal文档

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import JayUi from '@jayden-ui/components';
+import JayUi from 'jayden-ui';
 const app = createApp(App);
 
 app.use(JayUi);
--- site/docs/.vitepress/config.js
+++ site/docs/.vitepress/config.js
@@ -1,58 +1,62 @@
 export default {
-	title: 'jayden-ui',
-	base: process.env.NODE_ENV === 'production' ? '/jayden-ui/' : '/',
-	themeConfig: {
-		siteTitle: 'jayden-ui',
-		nav: [
-			{ text: '指南', link: '/guild/index' },
-			{ text: '组件', link: '/components/' },
-		],
-		socialLinks: [
-			{
-				icon: 'github',
-				link: 'https://github.com/SpinninJayWE/Jayden-UI',
-			},
-		],
-		sidebar: {
-			'/guild/': [
-				{
-					text: '基础',
-					items: [
-						{
-							text: '安装',
-							link: '/guild/index',
-						},
-						{
-							text: '快速教程',
-							link: '/guild/quickstart',
-						},
-					],
-				},
-				{
-					text: '进阶',
-					items: [
-						{
-							text: 'xx',
-							link: '/xx',
-						},
-					],
-				},
-			],
-			'/components/': [
-				{
-					text: '基础组件',
-					items: [
-						{
-							text: 'Icon',
-							link: '/components/icon/index',
-						},
-						{
-							text: 'Button',
-							link: '/components/button/index',
-						},
-					],
-				},
-			],
-		},
-	},
-}
+  title: 'jayden-ui',
+  base: process.env.NODE_ENV === 'production' ? '/jayden-ui/' : '/',
+  themeConfig: {
+    siteTitle: 'jayden-ui',
+    nav: [
+      { text: '指南', link: '/guild/index' },
+      { text: '组件', link: '/components/' }
+    ],
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/SpinninJayWE/Jayden-UI'
+      }
+    ],
+    sidebar: {
+      '/guild/': [
+        {
+          text: '基础',
+          items: [
+            {
+              text: '安装',
+              link: '/guild/index'
+            },
+            {
+              text: '快速教程',
+              link: '/guild/quickstart'
+            }
+          ]
+        },
+        {
+          text: '进阶',
+          items: [
+            {
+              text: 'xx',
+              link: '/xx'
+            }
+          ]
+        }
+      ],
+      '/components/': [
+        {
+          text: '基础组件',
+          items: [
+            {
+              text: 'Icon',
+              link: '/components/icon/index'
+            },
+            {
+              text: 'Button',
+              link: '/components/button/index'
+            },
+            {
+              text: 'Modal',
+              link: '/components/Modal/index'
+            }
+          ]
+        }
+      ]
+    }
+  }
+};
